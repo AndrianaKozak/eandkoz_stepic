8fe312c8b40a644f3a16062719b193f1ec846087
root <root@andriana-VirtualBox.(none)>
2016-03-16 15:41:06 +0200

    Code cleaned, gunicorn is running

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
-import urlparse
-#from wsgiref.simple_server import make_server
 
-def application (env, start_response):
+def application (environ, start_response):
     status = "200 OK"
     headers = [('content-Type', 'text/plain')]
-        start_response(status, headers)
-    parameters = environ['QUERY_STRING'].split('&')
-    data = ''
-    for param in parameters:
-        data += param + '\n'
+    start_response(status, headers)
+    data = environ['QUERY_STRING'].replace('&','\n')
+    print(data)
+    #data = ''
+    #for param in parameters:
+        #data += param + '\n'
 
     return [data]
 
